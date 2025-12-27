b978f22c1870a9fd6a1aee46643308022e6deb36
lucashenrique22 <lh834392@gmail.com>
2025-12-27 13:30:25 -0300

    refactor: ajusta a formatação e alinhamento da tabela de investimentos

--- banco-dcc061/resources/views/investments/index.blade.php
+++ banco-dcc061/resources/views/investments/index.blade.php
@@ -5,24 +5,24 @@
         </h2>
     </x-slot>
 
-    <div class="py-8 max-w-7xl mx-auto sm:px-6 lg:px-8">
+    <div class="pt-4 max-w-7xl mx-auto sm:px-6 lg:px-8">
         <div class="bg-white p-6 rounded shadow">
             <table class="w-full table-fixed">
                 <thead>
                     <tr class="border-b">
-                        <th class="text-left  w-1/4">Nome</th>
-                        <th class="text-left  w-1/4">Descrição</th>
-                        <th class="text-right w-1/4">Rentabilidade</th>
-                        <th class="text-right w-1/4">Mínimo</th>
+                        <th class="text-center w-1/4">Nome</th>
+                        <th class="text-center w-1/4">Tipo</th>
+                        <th class="text-center w-1/4">Rentabilidade</th>
+                        <th class="text-center w-1/4">Mínimo</th>
                     </tr>
                 </thead>
                 <tbody>
                     @foreach($investments as $investment)
                     <tr class="border-b">
-                        <td>{{ $investment->name }}</td>
-                        <td>{{ $investment->type }}</td>
-                        <td class="text-right">{{ $investment->profitability }}%</td>
-                        <td class="text-right">
+                        <td class="text-center">{{ $investment->name }}</td>
+                        <td class="text-center">{{ $investment->type }}</td>
+                        <td class="text-center">{{ $investment->profitability }}%</td>
+                        <td class="text-center">
                             R$ {{ number_format($investment->minimum_value, 2, ',', '.') }}
                         </td>
                     </tr>
