ada8cd5075ff1bf9ba2c5bc94cc6c24115da0371
lucashenrique22 <lh834392@gmail.com>
2025-12-30 19:41:57 -0300

    refactor: ajusta a formatação do cabeçalho em várias views

--- banco-dcc061/resources/views/admin/users/index.blade.php
+++ banco-dcc061/resources/views/admin/users/index.blade.php
@@ -1,6 +1,8 @@
 <x-app-layout>
     <x-slot name="header">
-        Gestão de Usuários
+        <h2 class="text-xl text-gray-800 leading-tight">
+            {{ __('Gestão de Usuários') }}
+        </h2>
     </x-slot>
 
     <div class="pt-4 max-w-7xl mx-auto">
